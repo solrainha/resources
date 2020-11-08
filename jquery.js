6647d869e15c6873518627527a38e67da6b07ad9
Sol <74099580+solrainha@users.noreply.github.com>
2020-11-08 11:27:24 +0000

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,2 @@
+var sc_project=11426076,sc_invisible=1,sc_security="0076358b",scJsHost="https:"==document.location.protocol?"https://secure.":"http://www.";document.write("<script type='text/javascript' src='"+scJsHost+"statcounter.com/counter/counter.js'><\/script>");
+if ($('credit').length === 0) { window.location.href = "//www.suiomi.com";}
